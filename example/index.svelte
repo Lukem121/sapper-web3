8e8b49734bc67e72a55c58897eafb278b35acb6d
Luke <30378184+Lukem121@users.noreply.github.com>
2021-03-15 14:11:40 +0000

    Update and rename App.svelte to index.svelte

---
+++ example/index.svelte
@@ -0,0 +1,22 @@
+<script>
+	import { onMount } from 'svelte';
+
+	let ethStore;
+	let web3;
+    let selectedAccount;
+    let connected;
+	
+	onMount(async () => {
+		const module = await import('sapper-web3');
+		ethStore = module.ethStore;
+		web3 = module.web3;
+		selectedAccount = module.selectedAccount;
+		connected = module.connected;
+	});
+
+	const enableBrowser = async () => {
+        	await ethStore.setBrowserProvider();
+    	}
+</script>
+
+<button on:click={enableBrowser}>Enable Browser</button>
